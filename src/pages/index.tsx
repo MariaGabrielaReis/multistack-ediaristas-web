eafe271d5b49280d1f356f90031e42422c45c76c
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-06-11 17:42:25 -0300

    Organização: criação de pastas e exclusão de arquivos iniciais
    Criação de pastas para melhor controle do projeto, além de exclusão dos arquivos iniciais e instalação de dependências, também criando o primeiro componente do projeto, 'ambiente seguro'

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
+
+export default function Home() {
+  return (
+    <div>
+     <SafeEnvironment/>
+    </div>
+  );
+}
