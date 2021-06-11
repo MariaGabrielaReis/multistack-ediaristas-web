1029922076cf256e391e13a494edc62fa2278a25
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2021-06-11 19:45:28 -0300

    Criação e estilização do componente de 'card' de diaristas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,22 @@
-import Head from "next/head";
 import SafeEnvironment from "ui/components/feedback/SafeEnvironment/SafeEnvironment";
 import PageTitle from "ui/components/data-display/PageTitle/PageTitle";
-
+import UserInformation from "ui/components/data-display/UserInformation/UserInformation";
 
 export default function Home() {
   return (
     <div>
-     <SafeEnvironment/>
-     <PageTitle 
+      <SafeEnvironment />
+      <PageTitle
         title={"Conheça os profissionais"}
-        subtitle={"Preencha seu endereço e veja todos os profissionais da sua localidade"}
+        subtitle={
+          "Preencha seu endereço e veja todos os profissionais da sua localidade"
+        }
+      />
+      <UserInformation
+        name={"Maby"}
+        picture={"https://github.com/MariaGabrielaReis.png"}
+        rating={5}
+        description={"São José dos Campos"}
       />
     </div>
   );
